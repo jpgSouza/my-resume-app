765482c4191e8a4b378677d785be5c8a416b795c
João Pedro <joao_giacometti@hotmail.com>
2020-10-01 19:52:21 -0300

    Setting app colors and font

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// {App Colors}
+const primaryColor = Color.fromARGB(255, 163, 210, 202);
+const secondaryColor = Color.fromARGB(255, 94, 170, 168);
+const buttonColor = Color.fromARGB(255, 5, 102, 118);
+
+// Primary font
+const font = 'Montserrat';
